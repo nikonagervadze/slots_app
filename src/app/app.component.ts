7715264041d05f07980c755826c489c80a533939
nikonagervadze <nikonagervadze@yahoo.com>
2024-02-20 04:13:56 +0400

    feat: Create App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SidebarContainerComponent } from './sidebar-container/sidebar-container.component';
+import { SlotsMainContainerComponent } from './slots-main-container/slots-main-container.component';
+import { SlotsFacade } from './+state/slots.facade';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SidebarContainerComponent,
+    SlotsMainContainerComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'slots-app';
+  private readonly slotsFacade = inject(SlotsFacade);
+  ngOnInit(): void {
+    this.slotsFacade.init();
+  }
 }
