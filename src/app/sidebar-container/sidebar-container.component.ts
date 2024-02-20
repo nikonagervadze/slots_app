2725c1e2c63c42c0a0ee914bad9300e374085eca
nikonagervadze <nikonagervadze@yahoo.com>
2024-02-20 04:15:06 +0400

    feat: Add sidebar container

---
+++ src/app/sidebar-container/sidebar-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar-container.component.html',
+  styleUrl: './sidebar-container.component.scss',
+})
+export class SidebarContainerComponent {}
